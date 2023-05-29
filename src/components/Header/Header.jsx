b826508c92778d1975da04babd1a123a5c6236e0
GabrielMatiasn10 <Gm4037003@gmail.com>
2023-05-29 20:07:56 -0300

    termino do heard

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,20 @@
+
 import React from 'react';
 
 function Header() {
   return (
     <header className="header">
       <div className="container">
-        <h1>opa</h1>
+        
+        <form>
+          <input
+            type="search"
+            placeholder="Buscar casa"
+            className="search__input"
+            required
+          />
+        </form>
+        <button type="submit" className="search__button"> icon</button>
       </div>
     </header>
   );
