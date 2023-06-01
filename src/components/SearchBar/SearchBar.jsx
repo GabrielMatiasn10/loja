6dd04f9b3eec15ed49b0f8c1f9b491a4c60d6c1f
GabrielMatiasn10 <Gm4037003@gmail.com>
2023-06-01 13:35:02 -0300

    atualizando barra de pesquisa

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -5,20 +5,20 @@ import './SearchBar.css';
 
 function SearchBar() {
 
-  const [searchValue, setSearchValue] = useState ('');
+  const [, setSearchValue] = useState ('');
 
   return (
     <div>
       <form className="search-bar">
         <input
           className="search__input"
-          value={searchValue}
+         
           type="search"
           placeholder="Buscar casa"
           onChange={({target}) => setSearchValue(target.value)}
           required
         />
-        {searchValue}
+       
         <button type="submit" className="search__button">
           <BiSearchAlt />
         </button>
