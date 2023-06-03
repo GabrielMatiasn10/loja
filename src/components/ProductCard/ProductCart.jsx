d17655cec64de5bfe9cb747c192a0a2c696643a7
GabrielMatiasn10 <Gm4037003@gmail.com>
2023-06-02 23:16:32 -0300

    productcart

--- src/components/ProductCard/ProductCart.jsx
+++ src/components/ProductCard/ProductCart.jsx
@@ -10,9 +10,14 @@ function ProductCart () {
         <h2 className="card__price">R$ 400</h2>
         <h2 className="card__title"> iphone</h2>
       </div>
-
+      <button type="button" className="button__add--card"></button>
     </section>
   );
 }
+
+
+
+
+
 
 export default ProductCart;
